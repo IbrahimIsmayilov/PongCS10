36551976d186e9e66bdfc4bd86aa910a522bee06
IbrahimIsmayilov <90726322+IbrahimIsmayilov@users.noreply.github.com>
2021-12-20 22:04:18 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -6,13 +6,21 @@ let ctx = cnv.getContext("2d");
 cnv.width = 800;
 cnv.height = 600;
 
-// Global Variables
 
+// Global Variables
+let wIsPressed = false;
+let aIsPressed = false;
+let dIsPressed = false; 
+let sIsPressed = false; 
+let arrowUp = false;
+let arrowDown = false;
+let arrowRight = false; 
+let arrowLeft = false; 
+let paddleY = 190;
+    let paddleY2 = 190;
 
 requestAnimationFrame(pong)
-
 function pong() {
-    // Update Variables
 
     // Draw the black background
     ctx.fillStyle = "black";
@@ -20,11 +28,36 @@ function pong() {
     
     // Draw the white line in the middle
     ctx.fillStyle = "white";
-    
-    let squareY = 0;
+    let squareY = 2;
     for  (n = 1; n <= 15; n++) {
         ctx.fillRect(380, squareY, 20, 20);
         squareY += 41;
     }
+
+    
+    
+
+
+    if (wIsPressed) {
+        paddleY++;
+    }
+
+    ctx.fillRect(20, paddleY, 15, 110); 
+    ctx.fillRect(765, paddleY2, 15, 110); 
+
+    
+    
+    
+    
+
     requestAnimationFrame(pong);
+} // How can the computer load code beyond this? Isn't the computer stuck in running this infinite function?
+
+document.addEventListener("keydown", keydownHandler);
+
+function keydownHandler(event) {
+    if (event.code = "keyW") {
+        !(wIsPressed);
+    }
+    
 }

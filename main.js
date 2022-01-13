f434f07a3d1e308ceb5cf27f6f7375826e71f8d1
IbrahimIsmayilov <90726322+IbrahimIsmayilov@users.noreply.github.com>
2022-01-13 14:48:57 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,232 +1,101 @@
-// Pong by Ibrahim Ismayilov
+// Simple Paint idea {add an option whether you want to draw with lines or circles or squares, adding more colors as options}
 
-// Setting up canvas
-let cnv = document.getElementById("canvas");
+// Set up canvas and graphics context
+let cnv = document.getElementById("myCanvas");
 let ctx = cnv.getContext("2d");
 cnv.width = 800;
 cnv.height = 600;
 
 // Global Variables
-let wIsPressed = false;
-let sIsPressed = false;
-let arrowUIsPressed = false;
-let arrowDIsPressed = false;
-let paddleY1 = 200;
-let paddleY2 = 200;
-let ballX = 350;
-let ballY = 160;
-let prevXVelocity;
-let xVelocity;
-let scoreLeft = 0;
-let scoreRight = 0;
-let frameCount = 0;
-let ballMoveFrame = 60;
-let twoBtn = document.getElementById("twoBtn");
-let oneBtn = document.getElementById("oneBtn");
-let easyBtn1p = document.getElementById("easyBtn1p");
-let mediumBtn1p = document.getElementById("mediumBtn1p");
-let hardBtn1p = document.getElementById("hardBtn1p");
-let easyBtn2p = document.getElementById("easyBtn2p");
-let mediumBtn2p = document.getElementById("mediumBtn2p");
-let hardBtn2p = document.getElementById("hardBtn2p");
-let openingText = document.getElementById("openingText");
-let oneSelection = document.getElementById("oneSelection");
-let twoSelection = document.getElementById("twoSelection");
-let yVelocity = Math.random() * 5;
+let mouseIsPressed = false
+let mouseX, mouseY, pmouseX, pmouseY;
+let size = 5;
+let penColor = "black";
 
-function pongTwoP() {
-  // Updating the frame count every 1/60th of a second
-  frameCount++;
+// ctx.lineCap = "round";
 
-  if (frameCount > ballMoveFrame) {
-  // Drawing the background
-  ctx.fillStyle = "black";
-  ctx.fillRect(0, 0, cnv.width, cnv.height);
+// Main Program Loop (60 FPS)
+requestAnimationFrame(loop);
 
-  // Drawing the line in the middle
-  ctx.fillStyle = "white";
-  let squareY = 15;
-  for (n = 1; n <= 30; n++) {
-    ctx.fillRect(380, squareY, 10, 10);
-    squareY += 35;
-  }
+function loop() {
 
-  // Drawing the ball
-  ctx.fillRect(ballX, ballY, 20, 20);
-  ballX += xVelocity;
-  ballY += yVelocity;
-
-  // Scoreboard
-  ctx.font = "50px Comic Sans MS, Comic Sans, cursive";
-  ctx.strokeStyle = "white";
-  ctx.strokeText(scoreLeft, 310, 70);
-  ctx.strokeText(scoreRight, 430, 70); //I dont understand why they are equally far apart from each other. I need the relationship between the distances of the two from the middle squares explained to me.
-
-  // Checking ball collision with the left paddle
-  if (ballX <= 35 && ballY > paddleY1 - 20 && ballY < paddleY1 + 100 || ballX >= 20 && ballY > paddleY1 - 20 && ballY < paddleY1 + 100 || ballY + 20 >= paddleY1 && ballX + 20 >= 20 && ballX <= 35) {
-    ballX *= -1;
-  }
-
-  // Checking ball collision with the right paddle
-  if (ballX > 745) {
-    if (ballY > paddleY2 - 20 && ballY < paddleY2 + 100) {
-      xVelocity *= -1;
-    } else {
-      // if (canvas.width < ballX < 810) {
-      //   scoreLeft++;
-      //   console.log(scoreLeft);
-      //   ballX = 820;
-      // If I run this code, why does the scoreLeft keep increasing?
-      if (ballX > canvas.width && frameCount < calcLimit) {
-        scoreLeft++;
-        xVelocity = 0;
-      }
-      frameCount++;
-      if (frameCount === 50) {
-        ballX = 350;
-        ballY = 160;
-        paddleY1 = 200;
-        paddleY2 = 200;
-        xVelocity = prevXVelocity;
-        yVelocity = Math.random() * 5;
-        while (yVelocity < 1.5) {
-        yVelocity = Math.random() * 5;
-        }
-        frameCount = 0;
-      }
+    // Draw a circle if mouseIsPressed
+    if (mouseIsPressed) {
+        ctx.strokeStyle = penColor;
+        ctx.lineWidth  = size;
+        ctx.beginPath();
+        ctx.moveTo(pmouseX, pmouseY);
+        ctx.lineTo(mouseX, mouseY);
+        ctx.stroke();
     }
-  }
 
-  // Checking ball collision with the top and bottom of the screen
-  if (ballY + 20 > canvas.height || ballY < 0) {
-    yVelocity *= -1;
-  }
-
-  // The S key to move the paddle down
-  if (sIsPressed && paddleY1 < 500) {
-    paddleY1 += 5.75;
-  }
-
-  // The W key to move the paddle up
-  if (wIsPressed && paddleY1 > 0) {
-    paddleY1 -= 5.75;
-  }
-
-  // The arrow up to move the paddle up
-  if (arrowDIsPressed && paddleY2 < 500) {
-    paddleY2 += 5.75;
-  }
-
-  // The arrow down to move the paddle down
-  if (arrowUIsPressed && paddleY2 > 0) {
-    paddleY2 -= 5.75;
-  }
-
-
-  // Drawing the paddles
-  ctx.fillRect(20, paddleY1, 15, 100);
-  ctx.fillRect(765, paddleY2, 15, 100);
-
-
-  requestAnimationFrame(pongTwoP);
-}
-}
-// How can the computer load code beyond this? Isn't the computer stuck in running this infinite function?
-
-function pongOneP() {
-  // Drawing the background
-  ctx.fillStyle = "black";
-  ctx.fillRect(0, 0, cnv.width, cnv.height);
-
-  // Drawing the white, bouncy parts
-  ctx.strokeStyle = "white";
-  ctx.strokeRect(0, 0, 25, cnv.height);
-  requestAnimationFrame(pongOneP);
-
+    requestAnimationFrame(loop);
 }
 
-// Event Listeners
-document.addEventListener("keydown", keydownHandler);
-document.addEventListener("keyup", keyupHandler);
-oneBtn.addEventListener("click", choseOneP);
-twoBtn.addEventListener("click", choseTwoP);
-easyBtn2p.addEventListener("click", easyPong2p);
-mediumBtn2p.addEventListener("click", mediumPong2p);
-hardBtn2p.addEventListener("click", hardPong2p);
-easyBtn1p.addEventListener("click", easyPong1p);
+// Document Event Stuff
+document.addEventListener("mousedown", mousedownHandler);
+document.addEventListener("mouseup", mouseupHandler);
+document.addEventListener("mousemove", mousemoveHandler)
+document.addEventListener("keydown", keydownHandler)
 
-
-function choseTwoP() {
-  openingText.classList.add("hidden");
-  twoSelection.classList.remove("hidden");
+function mousedownHandler(event) {
+    mouseIsPressed = true;
 }
 
-function choseOneP() {
-  openingText.classList.add("hidden");
-  oneSelection.classList.remove("hidden");
+function mouseupHandler() {
+    mouseIsPressed = false;
 }
 
-function easyPong2p() {
-  // let xVelocity = 7; Why does this not work?
-  twoSelection.classList.add("hidden");
-  cnv.classList.remove("hidden");
-  xVelocity = 6.5;
-  prevXVelocity = xVelocity;
-  calcLimit = 10;
-  pongTwoP();
+function mousemoveHandler(event) {
+    // Save previous mouseX and mouseY
+    pmouseX = mouseX;
+    pmouseY = mouseY;
+
+    // Update mouseX and mouseY
+    let cnvRect = cnv.getBoundingClientRect()
+    mouseX = event.x - cnvRect.x;
+    mouseY = event.y - cnvRect.y;
+    // console.log(event);
 }
 
-function mediumPong2p() {
-  twoSelection.classList.add("hidden");
-  cnv.classList.remove("hidden");
-  xVelocity = 8;
-  prevXVelocity = xVelocity;
-  calcLimit = 8;
-  pongTwoP();
-}
-
-function hardPong2p() {
-  twoSelection.classList.add("hidden");
-  cnv.classList.remove("hidden");
-  xVelocity = 9.5;
-  prevXVelocity = xVelocity;
-  calcLimit = 7;
-  pongTwoP();
-}
-
-function easyPong1p() {
-  oneSelection.classList.add("hidden");
-  cnv.classList.remove("hidden");
-  pongOneP();
-}
 function keydownHandler(event) {
-  // Checking if S or W key is pressed
-  if (event.code === "KeyS") {
-    sIsPressed = true;
-  } else if (event.code === "KeyW") {
-    wIsPressed = true;
-  }
+    console.log(event.code)
 
-  // Checking if the up or down arrow is pressed
-  if (event.code === "ArrowUp") {
-    arrowUIsPressed = true;
-  } else if (event.code === "ArrowDown") {
-    arrowDIsPressed = true;
-  }
+    if (event.code === "Space") { /// use three equals recommended means string data can only equal string and so forth very strict 2== means string data can equal string and number data
+        // Draw a background
+        ctx.fillStyle = "white";
+        ctx.fillRect(0, 0, cnv.width, cnv.height);
+    } else if (event.code === "ArrowUp") {
+        size++;
+    } else if (event.code === "ArrowDown") {
+        size--;
+    } else if (event.code === "Digit1") {
+        penColor = "red";
+    } else if (event.code === "Digit2") {
+        penColor = "green";
+    } else if (event.code === "Digit3") {
+        penColor = "blue";
+    }
+}
+ 
+// Color Events
+document.getElementById("redBtn").addEventListener("click", setRed);
+document.getElementById("greenBtn").addEventListener("click", setGreen);
+document.getElementById("blueBtn").addEventListener("click", setBlue);
+document.getElementById("colorPicker").addEventListener("change", changeColor);
+
+function setRed() {
+    penColor = "red";
 }
 
-function keyupHandler(event) {
-  // Checking if S or W key is released
-  if (event.code === "KeyS") {
-    sIsPressed = false;
-  } else if (event.code === "KeyW") {
-    wIsPressed = false;
-  }
+function setGreen() {
+    penColor = "green";
+}
 
-  // Checking if up or down arrow is released
-  if (event.code === "ArrowUp") {
-    arrowUIsPressed = false;
-  } else if (event.code === "ArrowDown") {
-    arrowDIsPressed = false;
-  }
+function setBlue() {
+    penColor = "blue";
+}
+
+function changeColor() {
+    penColor = document.getElementById("colorPicker").value;
 }

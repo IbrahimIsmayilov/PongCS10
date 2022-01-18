51c92a892ef4d5ac9e3f57ddd55a1dc3e83bd089
IbrahimIsmayilov <90726322+IbrahimIsmayilov@users.noreply.github.com>
2022-01-17 23:37:29 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -33,6 +33,7 @@ let frameCountTwo = 0;
 let hOne = document.getElementById("hOne");
 let hTwo = document.getElementById("hTwo");
 let randNum;
+let menuClick = document.getElementById("menuClick");
 
 
 requestAnimationFrame(animateText);
@@ -114,200 +115,208 @@ function pongTwoP() {
     if (ballX < 35 && ballX + 20 > 20 && ballY + 20 > paddleY1 && ballY < paddleY1 + 100) {
       xVelocity *= -1;
       if (yVelocity === 0) {
-        randNum = Math.random;
-        if (randNum > 0.5) {
-          while (yVelocity < 2)
-            yVelocity === Math.random() * 5;
+        randNum = Math.random();
+        if (randNum < 0.5) {
+          while (yVelocity <= 2) {
+            yVelocity = Math.random() * 5;
+          }
         } else {
-          while (yVelocity > -2)
-            yVelocity === Math.random() * -5;
+          while (yVelocity >= -2) {
+            yVelocity = Math.random() * -5;
+          }
         }
       }
     } else if (ballX < -20) {
-      xVelocity *= -1;
       scoreLeft++;
       ballMoveFrame = frameCount + 60;
+    }
 
-      // Checking ball collision with the right paddle
-      if (ballX + 20 > 765 && ballX < 780 && ballY + 20 > paddleY2 && ballY < paddleY2 + 100) {
-        xVelocity *= -1;
-        if (yVelocity === 0) {
-          randNum = Math.random;
-          if (randNum > 0.5) {
-            while (yVelocity < 2)
-              yVelocity === Math.random() * 5;
-          } else {
-            while (yVelocity > -2)
-              yVelocity === Math.random() * -5;
+    // Checking ball collision with the right paddle
+    if (ballX + 20 > 765 && ballX < 780 && ballY + 20 > paddleY2 && ballY < paddleY2 + 100) {
+      xVelocity *= -1;
+      if (yVelocity === 0) {
+        randNum = Math.random();
+        if (randNum < 0.5) {
+          while (yVelocity <= 2) {
+            yVelocity = Math.random() * 5;
+          }
+        } else {
+          while (yVelocity >= -2) {
+            yVelocity = Math.random() * -5;
           }
         }
-      } else if (ballX > 800) {
-        xVelocity *= -1;
-        scoreRight++;
-        ballMoveFrame = frameCount + 60;
       }
-
-      // Reverting the paddles back to their original positions after the ball has reappeared on the screen
-      if (frameCount === ballMoveFrame + 1) {
-        paddleY1 = 200;
-        paddleY2 = 200;
-      }
-
-      movePaddle();
-
-      // Drawing the ball
-      ctx.fillRect(ballX, ballY, 20, 20);
-      ballX += xVelocity;
-      ballY += yVelocity;
-
-      // Checking ball collision with the top and bottom of the screen
-      if (ballY + 20 > canvas.height || ballY < 0) {
-        yVelocity *= -1;
-      }
-
-    } else {
-      movePaddle();
-      yVelocity = 0;
-      ballX = 350;
-      ballY = 160;
+    } else if (ballX > 800) {
+      scoreRight++;
+      ballMoveFrame = frameCount + 60;
     }
 
-    requestAnimationFrame(pongTwoP);
+    // Reverting the paddles back to their original positions after the ball has reappeared on the screen
+    if (frameCount === ballMoveFrame + 1) {
+      paddleY1 = 200;
+      paddleY2 = 200;
+    }
+
+    movePaddle();
+
+    // Drawing the ball
+    ctx.fillRect(ballX, ballY, 20, 20);
+    ballX += xVelocity;
+    ballY += yVelocity;
+
+    // Checking ball collision with the top and bottom of the screen
+    if (ballY + 20 > canvas.height || ballY < 0) {
+      yVelocity *= -1;
+    }
+
+  } else {
+    movePaddle();
+    yVelocity = 0;
+    ballX = 350;
+    ballY = 160;
+    if (frameCount === ballMoveFrame - 1)
+      xVelocity *= -1;
   }
 
-  // How can the computer load code beyond this? Isn't the computer stuck in running this infinite function?
+  requestAnimationFrame(pongTwoP);
+}
+
+// How can the computer load code beyond this? Isn't the computer stuck in running this infinite function?
 
 
-  function movePaddle() {
-    // Drawing the background
-    ctx.fillStyle = "black";
-    ctx.fillRect(0, 0, cnv.width, cnv.height);
+function movePaddle() {
+  // Drawing the background
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, cnv.width, cnv.height);
 
-    // Drawing the line in the middle
-    ctx.fillStyle = "white";
-    let squareY = 15;
-    for (n = 1; n <= 30; n++) {
-      ctx.fillRect(380, squareY, 10, 10);
-      squareY += 35;
-    }
-
-    // Scoreboard
-    ctx.font = "50px Comic Sans MS, Comic Sans, cursive";
-    ctx.strokeStyle = "white";
-    ctx.strokeText(scoreLeft, 310, 70);
-    ctx.strokeText(scoreRight, 430, 70);
-
-    // The S key to move the paddle down
-    if (sIsPressed && paddleY1 < 500) {
-      paddleY1 += 5.75;
-    }
-
-    // The W key to move the paddle up
-    if (wIsPressed && paddleY1 > 0) {
-      paddleY1 -= 5.75;
-    }
-
-    // The arrow up to move the paddle up
-    if (arrowDIsPressed && paddleY2 < 500) {
-      paddleY2 += 5.75;
-    }
-
-    // The arrow down to move the paddle down
-    if (arrowUIsPressed && paddleY2 > 0) {
-      paddleY2 -= 5.75;
-    }
-
-    // Drawing the paddles
-    ctx.fillRect(20, paddleY1, 15, 100);
-    ctx.fillRect(765, paddleY2, 15, 100);
+  // Drawing the line in the middle
+  ctx.fillStyle = "white";
+  let squareY = 15;
+  for (n = 1; n <= 30; n++) {
+    ctx.fillRect(380, squareY, 10, 10);
+    squareY += 35;
   }
 
+  // Scoreboard
+  ctx.font = "50px Comic Sans MS, Comic Sans, cursive";
+  ctx.strokeStyle = "white";
+  ctx.strokeText(scoreLeft, 310, 70);
+  ctx.strokeText(scoreRight, 430, 70);
 
-  // Event Listeners
-  document.addEventListener("keydown", keydownHandler);
-  document.addEventListener("keyup", keyupHandler);
-  oneBtn.addEventListener("click", choseOneP);
-  twoBtn.addEventListener("click", choseTwoP);
-  easyBtn2p.addEventListener("click", easyPong2p);
-  mediumBtn2p.addEventListener("click", mediumPong2p);
-  hardBtn2p.addEventListener("click", hardPong2p);
-
-  function choseTwoP() {
-    hOne.classList.add("hidden");
-    hTwo.classList.add("hidden");
-    oneBtn.classList.add("hidden");
-    twoBtn.classList.add("hidden");
-    easyBtn2p.classList.remove("hidden");
-    mediumBtn2p.classList.remove("hidden");
-    hardBtn2p.classList.remove("hidden");
+  // The S key to move the paddle down
+  if (sIsPressed && paddleY1 < 500) {
+    paddleY1 += 5.75;
   }
 
-  function choseOneP() {
-    hOne.classList.add("hidden");
-    hTwo.classList.add("hidden");
-    oneBtn.classList.add("hidden");
-    twoBtn.classList.add("hidden");
-    easyBtn1p.classList.remove("hidden");
-    mediumBtn1p.classList.remove("hidden");
-    hardBtn1p.classList.remove("hidden");
+  // The W key to move the paddle up
+  if (wIsPressed && paddleY1 > 0) {
+    paddleY1 -= 5.75;
   }
 
-  function easyPong2p() {
-    xVelocity = -6;
-    easyBtn2p.classList.add("hidden");
-    mediumBtn2p.classList.add("hidden");
-    hardBtn2p.classList.add("hidden");
-    cnv.classList.remove("hidden");
-    pongTwoP();
+  // The arrow up to move the paddle up
+  if (arrowDIsPressed && paddleY2 < 500) {
+    paddleY2 += 5.75;
   }
 
-  function mediumPong2p() {
-    xVelocity = 7;
-    easyBtn2p.classList.add("hidden");
-    mediumBtn2p.classList.add("hidden");
-    hardBtn2p.classList.add("hidden");
-    cnv.classList.remove("hidden");
-    pongTwoP();
+  // The arrow down to move the paddle down
+  if (arrowUIsPressed && paddleY2 > 0) {
+    paddleY2 -= 5.75;
   }
 
-  function hardPong2p() {
-    xVelocity = 8;
-    easyBtn2p.classList.add("hidden");
-    mediumBtn2p.classList.add("hidden");
-    hardBtn2p.classList.add("hidden");
-    cnv.classList.remove("hidden");
-    pongTwoP();
+  // Drawing the paddles
+  ctx.fillRect(20, paddleY1, 15, 100);
+  ctx.fillRect(765, paddleY2, 15, 100);
+}
+
+
+// Event Listeners
+document.addEventListener("keydown", keydownHandler);
+document.addEventListener("keyup", keyupHandler);
+oneBtn.addEventListener("click", choseOneP);
+twoBtn.addEventListener("click", choseTwoP);
+easyBtn2p.addEventListener("click", easyPong2p);
+mediumBtn2p.addEventListener("click", mediumPong2p);
+hardBtn2p.addEventListener("click", hardPong2p);
+
+
+function choseTwoP() {
+  menuClick.currentTime = 0;
+  menuClick.play();
+  hOne.classList.add("hidden");
+  hTwo.classList.add("hidden");
+  oneBtn.classList.add("hidden");
+  twoBtn.classList.add("hidden");
+  easyBtn2p.classList.remove("hidden");
+  mediumBtn2p.classList.remove("hidden");
+  hardBtn2p.classList.remove("hidden");
+}
+
+function choseOneP() {
+  hOne.classList.add("hidden");
+  hTwo.classList.add("hidden");
+  oneBtn.classList.add("hidden");
+  twoBtn.classList.add("hidden");
+  easyBtn1p.classList.remove("hidden");
+  mediumBtn1p.classList.remove("hidden");
+  hardBtn1p.classList.remove("hidden");
+}
+
+function easyPong2p() {
+  xVelocity = -6;
+  easyBtn2p.classList.add("hidden");
+  mediumBtn2p.classList.add("hidden");
+  hardBtn2p.classList.add("hidden");
+  cnv.classList.remove("hidden");
+  pongTwoP();
+}
+
+function mediumPong2p() {
+  xVelocity = 7;
+  easyBtn2p.classList.add("hidden");
+  mediumBtn2p.classList.add("hidden");
+  hardBtn2p.classList.add("hidden");
+  cnv.classList.remove("hidden");
+  pongTwoP();
+}
+
+function hardPong2p() {
+  xVelocity = 8;
+  easyBtn2p.classList.add("hidden");
+  mediumBtn2p.classList.add("hidden");
+  hardBtn2p.classList.add("hidden");
+  cnv.classList.remove("hidden");
+  pongTwoP();
+}
+
+function keydownHandler(event) {
+  // Checking if S or W key is pressed
+  if (event.code === "KeyS") {
+    sIsPressed = true;
+  } else if (event.code === "KeyW") {
+    wIsPressed = true;
   }
 
-  function keydownHandler(event) {
-    // Checking if S or W key is pressed
-    if (event.code === "KeyS") {
-      sIsPressed = true;
-    } else if (event.code === "KeyW") {
-      wIsPressed = true;
-    }
-
-    // Checking if the up or down arrow is pressed
-    if (event.code === "ArrowUp") {
-      arrowUIsPressed = true;
-    } else if (event.code === "ArrowDown") {
-      arrowDIsPressed = true;
-    }
-
+  // Checking if the up or down arrow is pressed
+  if (event.code === "ArrowUp") {
+    arrowUIsPressed = true;
+  } else if (event.code === "ArrowDown") {
+    arrowDIsPressed = true;
   }
 
-  function keyupHandler(event) {
-    // Checking if S or W key is released
-    if (event.code === "KeyS") {
-      sIsPressed = false;
-    } else if (event.code === "KeyW") {
-      wIsPressed = false;
-    }
+}
 
-    // Checking if up or down arrow is released
-    if (event.code === "ArrowUp") {
-      arrowUIsPressed = false;
-    } else if (event.code === "ArrowDown") {
-      arrowDIsPressed = false;
-    }
+function keyupHandler(event) {
+  // Checking if S or W key is released
+  if (event.code === "KeyS") {
+    sIsPressed = false;
+  } else if (event.code === "KeyW") {
+    wIsPressed = false;
   }
+
+  // Checking if up or down arrow is released
+  if (event.code === "ArrowUp") {
+    arrowUIsPressed = false;
+  } else if (event.code === "ArrowDown") {
+    arrowDIsPressed = false;
+  }
+}

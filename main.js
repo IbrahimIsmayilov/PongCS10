a93f29e7cba4a2851142ebbecf7d8cbe9c19b829
IbrahimIsmayilov <90726322+IbrahimIsmayilov@users.noreply.github.com>
2022-01-27 01:47:59 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -50,7 +50,17 @@ let onePmodes = document.getElementById("1Pmodes");
 let twoPmodes = document.getElementById("2Pmodes");
 let singleMultiMode = document.getElementById("singleMultiMode");
 let multiPlay = false;
+let singlePlay = false;
 let textNotWritten = true;
+let frameCountThree = 0;
+let stageOne;
+let stageTwo;
+let stageThree;
+let stageFour;
+let easyAI = false;
+let mediumAI = false;
+let hardAI = false;
+
 
 requestAnimationFrame(animateText);
 
@@ -123,6 +133,145 @@ function animateText() {
   }
 }
 
+function pongOneP() {
+  // Updating the frame count every 1/60th of a second
+  frameCountThree++;
+
+  if (frameCountThree > ballMoveFrame) {
+
+    // Checking ball collision with the left paddle
+    if (ball.x < 35 && ball.x + 20 > 20 && ball.y + 20 > paddleY1 && ball.y < paddleY1 + 100) {
+      // Checking where the ball collides on the paddle
+      xVelocity *= -1;
+      ball.x = 35;
+      if (ball.y < paddleY1 && paddleY1 > ball.y + 10) {
+        ball.y = paddleY1 - 20;
+        if (yVelocity === 0) {
+          while (yVelocity > -2) {
+            yVelocity = Math.random() * -5;
+          }
+        } else {
+          if (2 < yVelocity < 5) {
+            yVelocity *= -1;
+          }
+        }
+      } else if (ball.y + 20 > paddleY1 + 100 && paddleY1 + 100 < ball.y + 10) {
+        ball.y = paddleY1 + 100;
+        if (yVelocity === 0) {
+          while (yVelocity < 2) {
+            yVelocity = Math.random() * 5;
+          }
+        } else {
+          if (-2 > yVelocity > -5) {
+            yVelocity *= -1;
+          }
+        }
+      } else {
+        randNum = Math.random();
+        if (yVelocity === 0) {
+          if (randNum < 0.5) {
+            while (yVelocity > -2) {
+              yVelocity = Math.random() * -5;
+            }
+          } else {
+            while (yVelocity < 2) {
+              yVelocity = Math.random() * 5;
+            }
+          }
+        }
+      }
+    } else if (ball.x < -20) {
+      scoreLeft++;
+      ballMoveFrame = frameCountThree + 60;
+    }
+
+    // Checking ball collision with the right paddle
+    if (ball.x + 20 > 765 && ball.x < 780 && ball.y + 20 > paddleY2 && ball.y < paddleY2 + 100) {
+      // Checking where the ball collides on the paddle
+      ball.x = 745;
+      xVelocity *= -1;
+      if (ball.y < paddleY2 && paddleY2 > ball.y + 10) {
+        ball.y = paddleY2 - 20;
+        if (yVelocity === 0) {
+          while (yVelocity > -2) {
+            yVelocity = Math.random() * -5;
+          }
+        } else {
+          if (2 < yVelocity < 5) {
+            yVelocity *= -1;
+          }
+        }
+      } else if (ball.y + 20 > paddleY2 + 100 && paddleY2 + 100 < ball.y + 10) {
+        ball.y = paddleY2 + 100;
+        if (yVelocity === 0) {
+          while (yVelocity < 2) {
+            yVelocity = Math.random() * 5;
+          }
+        } else {
+          if (-2 > yVelocity > -5) {
+            yVelocity *= -1;
+          }
+        }
+      } else {
+        randNum = Math.random();
+        if (yVelocity === 0) {
+          if (randNum < 0.5) {
+            while (yVelocity > -2) {
+              yVelocity = Math.random() * -5;
+            }
+          } else {
+            while (yVelocity < 2) {
+              yVelocity = Math.random() * 5;
+            }
+          }
+        }
+      }
+    } else if (ball.x > 800) {
+      scoreRight++;
+      ballMoveFrame = frameCountThree + 60;
+    }
+
+    // 
+
+    // Reverting the paddles back to their original positions after the ball has reappeared on the screen
+    if (frameCountThree === ballMoveFrame + 1) {
+      paddleY1 = 200;
+      paddleY2 = 200;
+    }
+
+    // The function to keep track of the scoreboard, move the paddles and so forth. Made into a function because it will be reused later in the program. 
+    movePaddleOneP();
+
+    // Drawing the ball
+    ctx.fillRect(ball.x, ball.y, 20, 20);
+    ball.x += xVelocity;
+    ball.y += yVelocity;
+
+    // Checking ball collision with the top and bottom of the screen
+    if (ball.y + 20 > canvas.height || ball.y < 0) {
+      yVelocity *= -1;
+    }
+
+  } else {
+    movePaddleOneP(); // Specificially made into a function so that the player is able to move the paddles for a second even after the ball is off-screen. Makes the whole game feel more natural. 
+    yVelocity = 0;
+    ball.x = 350;
+    ball.y = 160;
+    if (frameCountThree === ballMoveFrame - 1) // This is the last time this function is run before the if statement above in which if (frameCount > ballMoveFrame) equals true. Thus, this change in xVelocity is only going to run once and it is programmed to start the game with the ball heading towards the player who scored similar to how a soccer game starts after a goal is scored.  
+      xVelocity *= -1;
+  }
+  if (singlePlay) {
+    requestAnimationFrame(pongOneP);
+  } else {
+    yVelocity = 0;
+    ball.x = 350;
+    ball.y = 160;
+    paddleY1 = 200;
+    paddleY2 = 200;
+  }
+}
+
+
 function pongTwoP() {
   // Updating the frame count every 1/60th of a second
   frameCount++;
@@ -133,22 +282,23 @@ function pongTwoP() {
     if (ball.x < 35 && ball.x + 20 > 20 && ball.y + 20 > paddleY1 && ball.y < paddleY1 + 100) {
       // Checking where the ball collides on the paddle
       xVelocity *= -1;
-      if (ball.y + 20 < paddleY1) {
+      ball.x = 35;
+      if (ball.y < paddleY1 && paddleY1 > ball.y + 10) {
         ball.y = paddleY1 - 20;
         if (yVelocity === 0) {
-          while (yVelocity < 2) {
-            yVelocity = Math.random() * 5;
+          while (yVelocity > -2) {
+            yVelocity = Math.random() * -5;
           }
         } else {
           if (2 < yVelocity < 5) {
             yVelocity *= -1;
           }
         }
-      } else if (ball.y > paddleY1 + 100) {
+      } else if (ball.y + 20 > paddleY1 + 100 && paddleY1 + 100 < ball.y + 10) {
         ball.y = paddleY1 + 100;
         if (yVelocity === 0) {
-          while (yVelocity > -2) {
-            yVelocity = Math.random() * -5;
+          while (yVelocity < 2) {
+            yVelocity = Math.random() * 5;
           }
         } else {
           if (-2 > yVelocity > -5) {
@@ -159,12 +309,15 @@ function pongTwoP() {
         randNum = Math.random();
         if (yVelocity === 0) {
           if (randNum < 0.5) {
-            yVelocity = Math.random() * -5;
+            while (yVelocity > -2) {
+              yVelocity = Math.random() * -5;
+            }
           } else {
-            yVelocity = Math.random() * 5;
+            while (yVelocity < 2) {
+              yVelocity = Math.random() * 5;
+            }
           }
         }
-        ball.x = 35;
       }
     } else if (ball.x < -20) {
       scoreLeft++;
@@ -174,23 +327,24 @@ function pongTwoP() {
     // Checking ball collision with the right paddle
     if (ball.x + 20 > 765 && ball.x < 780 && ball.y + 20 > paddleY2 && ball.y < paddleY2 + 100) {
       // Checking where the ball collides on the paddle
+      ball.x = 745;
       xVelocity *= -1;
-      if (ball.y + 20 < paddleY2) {
+      if (ball.y < paddleY2 && paddleY2 > ball.y + 10) {
         ball.y = paddleY2 - 20;
         if (yVelocity === 0) {
-          while (yVelocity < 2) {
-            yVelocity = Math.random() * 5;
+          while (yVelocity > -2) {
+            yVelocity = Math.random() * -5;
           }
         } else {
           if (2 < yVelocity < 5) {
             yVelocity *= -1;
           }
         }
-      } else if (ball.y > paddleY2 + 100) {
+      } else if (ball.y + 20 > paddleY2 + 100 && paddleY2 + 100 < ball.y + 10) {
         ball.y = paddleY2 + 100;
         if (yVelocity === 0) {
-          while (yVelocity > -2) {
-            yVelocity = Math.random() * -5;
+          while (yVelocity < 2) {
+            yVelocity = Math.random() * 5;
           }
         } else {
           if (-2 > yVelocity > -5) {
@@ -201,12 +355,15 @@ function pongTwoP() {
         randNum = Math.random();
         if (yVelocity === 0) {
           if (randNum < 0.5) {
-            yVelocity = Math.random() * -5;
+            while (yVelocity > -2) {
+              yVelocity = Math.random() * -5;
+            }
           } else {
-            yVelocity = Math.random() * 5;
+            while (yVelocity < 2) {
+              yVelocity = Math.random() * 5;
+            }
           }
         }
-        ball.x = 745;
       }
     } else if (ball.x > 800) {
       scoreRight++;
@@ -220,7 +377,7 @@ function pongTwoP() {
     }
 
     // The function to keep track of the scoreboard, move the paddles and so forth. Made into a function because it will be reused later in the program. 
-    movePaddle();
+    movePaddleTwoP();
 
     // Drawing the ball
     ctx.fillRect(ball.x, ball.y, 20, 20);
@@ -233,7 +390,7 @@ function pongTwoP() {
     }
 
   } else {
-    movePaddle(); // Specificially made into a function so that the player is able to move the paddles for a second even after the ball is off-screen. Makes the whole game feel more natural. 
+    movePaddleTwoP(); // Specificially made into a function so that the player is able to move the paddles for a second even after the ball is off-screen. Makes the whole game feel more natural. 
     yVelocity = 0;
     ball.x = 350;
     ball.y = 160;
@@ -251,7 +408,7 @@ function pongTwoP() {
   }
 }
 
-function movePaddle() {
+function movePaddleTwoP() {
   // Drawing the background
   ctx.fillStyle = "black";
   ctx.fillRect(0, 0, cnv.width, cnv.height);
@@ -260,7 +417,7 @@ function movePaddle() {
   ctx.fillStyle = "white";
   let squareY = 15;
   for (n = 1; n <= 30; n++) {
-    ctx.fillRect(380, squareY, 10, 10);
+    ctx.fillRect(390, squareY, 10, 10);
     squareY += 35;
   }
 
@@ -272,22 +429,22 @@ function movePaddle() {
 
   // The S key to move the paddle down
   if (sIsPressed && paddleY1 < 500) {
-    paddleY1 += 5.75;
+    paddleY1 += 6.15;
   }
 
   // The W key to move the paddle up
   if (wIsPressed && paddleY1 > 0) {
-    paddleY1 -= 5.75;
+    paddleY1 -= 6.15;
   }
 
   // The arrow up to move the paddle up
   if (arrowDIsPressed && paddleY2 < 500) {
-    paddleY2 += 5.75;
+    paddleY2 += 6.15;
   }
 
   // The arrow down to move the paddle down
   if (arrowUIsPressed && paddleY2 > 0) {
-    paddleY2 -= 5.75;
+    paddleY2 -= 6.15;
   }
 
   // Drawing the paddles
@@ -295,6 +452,39 @@ function movePaddle() {
   ctx.fillRect(765, paddleY2, 15, 100);
 }
 
+function movePaddleOneP() {
+  // Drawing the background
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, cnv.width, cnv.height);
+
+  // Drawing the line in the middle
+  ctx.fillStyle = "white";
+  let squareY = 15;
+  for (n = 1; n <= 30; n++) {
+    ctx.fillRect(390, squareY, 10, 10);
+    squareY += 35;
+  }
+
+  // Scoreboard
+  ctx.font = "50px Comic Sans MS, Comic Sans, cursive";
+  ctx.strokeStyle = "white";
+  ctx.strokeText(scoreLeft, 310, 70);
+  ctx.strokeText(scoreRight, 430, 70);
+
+  // The S key to move the paddle down
+  if (sIsPressed && paddleY1 < 500) {
+    paddleY1 += 6.15;
+  }
+
+  // The W key to move the paddle up
+  if (wIsPressed && paddleY1 > 0) {
+    paddleY1 -= 6.15;
+  }
+
+  // Drawing the paddles
+  ctx.fillRect(20, paddleY1, 15, 100);
+  ctx.fillRect(765, paddleY2, 15, 100);
+}
 
 // Event Listeners
 document.addEventListener("keydown", keydownHandler);
@@ -305,6 +495,9 @@ easyBtn2p.addEventListener("click", easyPong2p);
 mediumBtn2p.addEventListener("click", mediumPong2p);
 hardBtn2p.addEventListener("click", hardPong2p);
 returnBtnEl.addEventListener("click", returnBtn);
+easyBtn1p.addEventListener("click", easyPong1p);
+mediumBtn1p.addEventListener("click", mediumPong1p);
+hardBtn1p.addEventListener("click", hardPong1p);
 
 // What the return button does
 function returnBtn() {
@@ -323,6 +516,14 @@ function returnBtn() {
     returnBtnEl.classList.remove("hidden");
     singleMultiMode.classList.add("hidden");
     twoPmodes.classList.remove("hidden");
+  }
+
+  if (singlePlay) {
+    singlePlay = false;
+    returnBtnEl.classList.remove("hidden");
+    cnv.classList.add("hidden");
+    singleMultiMode.classList.add("hidden");
+    onePmodes.classList.remove("hidden");
   }
 }
 
@@ -352,18 +553,18 @@ function choseOneP() {
 function easyPong2p() {
   menuClick.currentTime = 0;
   menuClick.play();
-  xVelocity = 6;
+  xVelocity = 8;
   twoPmodes.classList.add("hidden");
   cnv.classList.remove("hidden");
   multiPlay = true;
-  pongTwoP();
+  pongOneP();
 }
 
 // Medium level pong in multiplayer mode
 function mediumPong2p() {
   menuClick.currentTime = 0;
   menuClick.play();
-  xVelocity = 7;
+  xVelocity = 8.5;
   twoPmodes.classList.add("hidden");
   cnv.classList.remove("hidden");
   multiPlay = true;
@@ -374,11 +575,42 @@ function mediumPong2p() {
 function hardPong2p() {
   menuClick.currentTime = 0;
   menuClick.play();
-  xVelocity = 8;
+  xVelocity = 9;
   twoPmodes.classList.add("hidden");
   cnv.classList.remove("hidden");
   multiPlay = true;
   pongTwoP();
+}
+
+// Easy level pong in singleplayer mode
+function easyPong1p() {
+  menuClick.currentTime = 0;
+  menuClick.play();
+  xVelocity = 8;
+  onePmodes.classList.add("hidden");
+  cnv.classList.remove("hidden");
+  singlePlay = true;
+  pongOneP();
+}
+
+function mediumPong1p() {
+  menuClick.currentTime = 0;
+  menuClick.play();
+  xVelocity = 8.5;
+  onePmodes.classList.add("hidden");
+  cnv.classList.remove("hidden");
+  singlePlay = true;
+  pongOneP();
+}
+
+function hardPong1p() {
+  menuClick.currentTime = 0;
+  menuClick.play();
+  xVelocity = 9;
+  onePmodes.classList.add("hidden");
+  cnv.classList.remove("hidden");
+  singlePlay = true;
+  pongOneP();
 }
 
 // Checking which keys are pressed
@@ -412,8 +644,5 @@ function keyupHandler(event) {
     arrowUIsPressed = false;
   } else if (event.code === "ArrowDown") {
     arrowDIsPressed = false;
-
-
   }
 }
-
